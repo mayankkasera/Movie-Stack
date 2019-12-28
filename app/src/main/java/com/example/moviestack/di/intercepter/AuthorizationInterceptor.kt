0719492844b81709345c38f15ca8a71bb704699e
Happy Singh <happysingh23828@gmail.com>
2019-12-28 17:03:29 +0530

    TMDB API_KEY privacy added

--- app/src/main/java/com/example/moviestack/di/intercepter/AuthorizationInterceptor.kt
+++ app/src/main/java/com/example/moviestack/di/intercepter/AuthorizationInterceptor.kt
@@ -1,6 +1,6 @@
 package com.example.moviestack.di.intercepter
 
-import com.example.moviestack.utils.NetworkConstants
+import com.example.moviestack.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -8,7 +8,7 @@ class AuthorizationInterceptor : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
-        val newHttpUrl = originalRequest.url().newBuilder().addQueryParameter("api_key",NetworkConstants.TMDB_API_KEY).build()
+        val newHttpUrl = originalRequest.url().newBuilder().addQueryParameter("api_key",BuildConfig.TMDB_API_KEY).build()
         val newRequest = originalRequest.newBuilder().url(newHttpUrl).build()
         return chain.proceed(newRequest)
     }
