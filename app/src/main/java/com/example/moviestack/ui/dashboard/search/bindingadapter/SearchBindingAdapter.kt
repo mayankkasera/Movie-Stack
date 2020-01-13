2cceecc16d9d05b5c2b8e2eca499e5e2abe0a984
mayankkasera <mayankkasera98@gmail.com>
2020-01-13 12:53:32 +0530

    Search Fragment Completed.

--- app/src/main/java/com/example/moviestack/ui/dashboard/search/bindingadapter/SearchBindingAdapter.kt
+++ app/src/main/java/com/example/moviestack/ui/dashboard/search/bindingadapter/SearchBindingAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.moviestack.ui.dashboard.search.bindingadapter
 
 import android.content.Intent
+import android.widget.LinearLayout
 import androidx.cardview.widget.CardView
 import androidx.databinding.BindingAdapter
 import com.example.moviestack.ui.common.ListType
 import com.example.moviestack.ui.common.movielist.paginglist.MovieListPaggingActivity
 import com.example.moviestack.ui.common.person.paging.PersonPagingActivity
+import com.example.moviestack.ui.search.SearchActivity
 
 object SearchBindingAdapter {
 
@@ -21,9 +23,18 @@ object SearchBindingAdapter {
 
     @JvmStatic
     @BindingAdapter("setPersonOnclick")
-    fun setPersonOnclick (layout : CardView, type : ListType.Type?){
+    fun setPersonOnclick (layout : CardView, any : Any?){
         layout.setOnClickListener{
             val intent = Intent(layout.context, PersonPagingActivity::class.java)
+            layout.context.startActivity(intent)
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("setGoToActivityOnclick")
+    fun setGoToActivityOnclick (layout : LinearLayout, any : Any?){
+        layout.setOnClickListener{
+            var intent = Intent(layout.context, SearchActivity::class.java)
             layout.context.startActivity(intent)
         }
     }
